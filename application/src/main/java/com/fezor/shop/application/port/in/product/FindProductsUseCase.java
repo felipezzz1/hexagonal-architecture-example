85a806f084e6d059218d1911b9f63aa4b93c1d95
felipezzz1 <felipezorzo78@gmail.com>
2026-07-19 20:54:19 -0300

    chore: pom.xml adjustments to use model in application module

--- application/src/main/java/com/fezor/shop/application/port/in/product/FindProductsUseCase.java
+++ application/src/main/java/com/fezor/shop/application/port/in/product/FindProductsUseCase.java
@@ -1,4 +1,10 @@
 package com.fezor.shop.application.port.in.product;
 
+import com.fezor.shop.model.product.Product;
+
+import java.util.List;
+
 public interface FindProductsUseCase {
+
+    List<Product>
 }
