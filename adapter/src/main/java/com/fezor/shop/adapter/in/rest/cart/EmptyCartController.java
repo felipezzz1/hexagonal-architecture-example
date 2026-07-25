fe5fb044a240d75877335836342f0a11e27f5e38
felipezzz1 <felipezorzo78@gmail.com>
2026-07-24 22:16:48 -0300

    feat: Emptying the shopping cart adapter

---
+++ adapter/src/main/java/com/fezor/shop/adapter/in/rest/cart/EmptyCartController.java
@@ -0,0 +1,30 @@
+package com.fezor.shop.adapter.in.rest.cart;
+
+import static com.fezor.shop.adapter.in.rest.common.CustomerIdParser.parseCustomerId;
+
+import com.fezor.shop.application.port.in.cart.EmptyCartUseCase;
+import com.fezor.shop.model.customer.CustomerId;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/carts")
+@Produces(MediaType.APPLICATION_JSON)
+public class EmptyCartController {
+
+    private final EmptyCartUseCase emptyCartUseCase;
+
+    public EmptyCartController(EmptyCartUseCase emptyCartUseCase) {
+        this.emptyCartUseCase = emptyCartUseCase;
+    }
+
+    @DELETE
+    @Path("/{customerId}")
+    public void deleteCart(@PathParam("customerId") String customerIdString) {
+        CustomerId customerId = parseCustomerId(customerIdString);
+
+        emptyCartUseCase.emptyCart(customerId);
+    }
+}
