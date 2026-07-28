6bf846263f38e1fba0c8bc9a6e16aa2b042e4738
felipezzz1 <felipezorzo78@gmail.com>
2026-07-27 21:37:18 -0300

    chore: add restassured to bootstrap module pom.xml

--- bootstrap/src/test/java/com/fezor/shop/bootstrap/e2e/CartTest.java
+++ bootstrap/src/test/java/com/fezor/shop/bootstrap/e2e/CartTest.java
@@ -1,6 +1,6 @@
 package com.fezor.shop.bootstrap.e2e;
 
-import static com.fezor.shop.adapter.in.rest.TEST_PORT;
+import static com.fezor.shop.adapter.in.rest.HttpTestCommons.TEST_PORT;
 import static com.fezor.shop.adapter.in.rest.cart.CartsControllerAssertions.assertThatResponseIsCart;
 import static com.fezor.shop.adapter.out.persistence.DemoProducts.LED_LIGHTS;
 import static com.fezor.shop.adapter.out.persistence.DemoProducts.MONITOR_DESK_MOUNT;
