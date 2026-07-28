c1132af57329707beff3b989262e1dcc411fdfce
felipezzz1 <felipezorzo78@gmail.com>
2026-07-27 21:22:32 -0300

    chore: add junit to pom.ml in the bootstrap module to start implementing the e2e tests

---
+++ bootstrap/src/test/java/com/fezor/shop/bootstrap/e2e/CartTest.java
@@ -0,0 +1,11 @@
+package com.fezor.shop.bootstrap.e2e;
+
+import static com.fezor.shop.adapter.in.rest.TEST_PORT;
+import static com.fezor.shop.adapter.in.rest.cart.CartsControllerAssertions.assertThatResponseIsCart;
+import static com.fezor.shop.adapter.out.persistence.DemoProducts.LED_LIGHTS;
+import static com.fezor.shop.adapter.out.persistence.DemoProducts.MONITOR_DESK_MOUNT;
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.Response.Status.NO_CONTENT;
+
+public class CartTest {
+}
---
+++ bootstrap/src/test/java/com/fezor/shop/bootstrap/e2e/EndToEndTest.java
@@ -0,0 +1,11 @@
+package com.fezor.shop.bootstrap.e2e;
+
+import com.fezor.shop.bootstrap.Launcher;
+
+abstract class EndToEndTest {
+
+    private static Launcher launcher;
+
+    @BeforeAll
+
+}
